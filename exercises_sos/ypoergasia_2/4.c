63cc0fb2b88954235c01b748697b2605589e7784
gmoraitis <moraitis.georgios@icloud.com>
2020-02-22 18:41:27 +0200

    Changes for execrise 4

--- exercises_sos/ypoergasia_2/4.c
+++ exercises_sos/ypoergasia_2/4.c
@@ -6,7 +6,7 @@ const int kommatia_endeikti[13][7] = { {1,1,1,1,1,1,0}, {0,1,1,0,0,0,0}, {1,1,0,
 {0,1,1,0,0,1,1}, {1,0,1,1,0,1,1}, {1,0,1,1,1,1,1}, {1,1,1,0,0,0,0}, 
 {1,1,1,1,1,1,1}, {1,1,1,1,0,1,1,}, {1,1,1,0,1,1,1}, {1,0,0,1,1,1,0}, {1,0,0,0,1,1,1,}}; 
 
-char eksodos[4][megistos_arithmos_eisodou * 4] = {"-","|","|","_","|","|","-"}; ///ta symvola ta vazoyme edw h ston 3x3 ?? Pisteuw edw giati exei mas ton exoyn dhlvsei me char sthn ekfvnhsh.
+char eksodos[4][megistos_arithmos_eisodou * 4]; 
 const int thesi_kommation[7][2] = {0,1,1,0,1,1,1,2,2,0,2,1,2,2};
 int array[3][3]; 
 int input = 0;
@@ -18,18 +18,25 @@ void adeiasma_pinaka_eksodou(void)
 {
     for (i = 0; i < 4; i = i + 1)
     {
-        for (j = 0; j < megistos_arithmos_eisodou * 4; j = j + 1)
+        for (j = 0; j < megistos_arithmos_eisodou * 4; j++)
             eksodos[i][j] = ' ';
     }
 }
 
-void epeksergasia_eisodou(int eisodos, int thesi)
-{
-    for (i = 0; i < megistos_arithmos_eisodou - 1; i++)
-    {
-        eksodos[thesi_kommation[i][0]][thesi_kommation[i][1]]  =  kommatia_endeikti[eisodos][i] ;
-    }
-}
+void epeksergasia_eisodou(int eisodos, int thesi) 
+	{
+	   int i, grammes, stiles;
+	   
+	   for (i = 0; i < 7; i++) 
+	   {
+	       if (kommatia_endeikti[eisodos][i]) 
+		   {
+	           grammes = thesi_kommation[i][0];
+	           stiles = thesi_kommation[i][1] + thesi;
+	           eksodos[grammes][stiles] = i % 3 == 0 ? '_' : '|';
+	       }
+	   }
+	}
 
 
 
@@ -44,21 +51,22 @@ void ektypwsh_eksodou(void)
 
 int main()
 {
-    adeiasma_pinaka_eksodou();//Logika prepei na valoume sthn arxh aythn thn synarthsh swsta?
+    adeiasma_pinaka_eksodou();
     printf("Εισάγετε δέκα χαρακτήρες από 0-9 και A, C, F: ");
-    while ((input = getchar()) != '\n' && count < megistos_arithmos_eisodou)//Pernoume xarakthres mexrh na mhn vroume keno xarakrhra --> ("\n")?Kai epeita mexrh o count na ginei 10.
+    while ((input = getchar()) != '\n' && count < megistos_arithmos_eisodou)
     {
         count += 1;
-        if (isdigit(input))//Elegxoume to input sthn isdigit kai ean einai pshfio kanoume ta akoloutha *alliws...
+        if (isdigit(input))
         {   
             
-            epeksergasia_eisodou(input,count);//To input einai o kathe xarakthras.To count einai enas metrhths alla antoistoixa einai kai h thesh tou eksodos kathe fora??
-            input = input + 4;//Ean metaferei kata 4 xarakthres thn eisagvgh tou kathe input.Prepei na mpei edW H sto telos? 
-            void ektypwsh_eksodou(void);//Tha pernei san orizma kati apo ton pinaka eksodos?
+            epeksergasia_eisodou(input,count);
+            input = input + 4;
+            ektypwsh_eksodou();
         }
-        else//* alliws ean einai xarakthras ta ksanastrexoume ola ayta pou grapsame sthn if alla gia tous xarakthres A,C,F ?
+        else
         {
         }
     }
 }
 
+ 
